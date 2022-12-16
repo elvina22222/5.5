2838f6a7b78fe614ad2953ae94538323e45d82a4
elvina22222 <114909756+elvina22222@users.noreply.github.com>
2022-12-16 10:32:02 +0400

    create put

--- putdel/src/server.js
+++ putdel/src/server.js
@@ -7,6 +7,30 @@ const user = [
     [id: 1 , name: 'B' , position: "boss"],
     [id: 2 , name:'C' , position: "cleaner"]
 ]; 
+app.put("/user/:id", function (req, res) {
+    const idOfUser = parseInt(req.body.id);
+    const userIdx = users.findIndex((user) => user.id === idOfUser);
+  
+    if (userIdx !== -1) {
+      const oldUser = users[userIdx];
+      users[userIdx] = { ...oldUser, ...req.body };
+      res.json(users[userIdx]);
+    } else {
+      res.status(404).json();
+    }
+  });
 
 
+  app.put("/user/:id", function (req, res) {
+    const idOfUser = parseInt(req.body.id);
+    const userIdx = users.findIndex((user) => user.id === idOfUser);
+  
+    if (userIdx !== -1) {
+      const oldUser = users[userIdx];
+      users[userIdx] = { ...oldUser, ...req.body };
+      res.json(users[userIdx]);
+    } else {
+      res.status(404).json();
+    }
+  });
 }
