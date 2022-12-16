c6006205ac626ec3bc71be32e128ffe2b1112ac4
elvina22222 <114909756+elvina22222@users.noreply.github.com>
2022-12-16 10:30:17 +0400

    create massiv

---
+++ putdel/src/server.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+export default class Info extends Component {
+const express = require('express');
+const app = express();
+const user = [
+    [id: 0, name : 'A' , position: "manager"],
+    [id: 1 , name: 'B' , position: "boss"],
+    [id: 2 , name:'C' , position: "cleaner"]
+]; 
+
+
+}
